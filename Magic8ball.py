da238bc10fc2d4c56d81414f04489b4cb108b2ab
FrazerMichael <80073219+FrazerMichael@users.noreply.github.com>
2021-03-20 13:21:31 -0400

    Add files via upload

---
+++ Magic8ball.py
@@ -0,0 +1,10 @@
+import random
+
+answers = ['As I see it, yes.', 'Ask again later.', 'Better not tell you now.',
+    'Concentrate and ask again.', 'Dount coun on it', 'It is decidedly so.']
+
+print('Ask the magic 8 ball a question.')
+
+input()
+
+print(random.choice(answers))
